1f95c50e917cf59e737cecff3f8aeb0d53ac3038
Sergey Sofeychuk <lolwat97@gmail.com>
2020-04-13 13:54:09 +0300

    flashing&audio

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var audio = new Audio('./bgm.mp3');
+audio.volume = 0.5;
+
+audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+
 function showLove(){
     alert("ppl be groovin");
     document.body.style.background = "url('dance4.gif') repeat";
-    document.getElementById("tehbutton").style.display = 'none';
+    document.getElementById("tehbutton").style.visibility = 'hidden';
+    document.getElementById("textflash").classList.add("flashing");
+    audio.play();
 }
